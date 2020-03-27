487d4ae2acf57d411d95a85dd4d01dd39c0f061f
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2020-03-27 15:38:04 -0400

    add multiple emails

--- mail-handler.php
+++ mail-handler.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "austinbarrington@gmail.com"; // this is your Email address
+    $to = "your_email1, your_email2"; // this is your Email address - enter where you want notification sent
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $hub_name = $_POST['hub_name'];
@@ -19,4 +19,5 @@ exit();
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
+    
 ?>
