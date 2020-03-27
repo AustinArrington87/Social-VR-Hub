11cc6b47901895fe8565f0cb31aceeeeb3508d77
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2020-03-27 15:00:54 -0400

    added method for new users to submit embed link to curators

--- iframe.php
+++ iframe.php
@@ -1,13 +1,46 @@
 <html>
 <body>
 <div align="center">
-Welcome to <?php echo $_POST["hub_name"]; ?><br></br>
-<?php 
+    
+<section>
+    <div align="center">
+        <h1 class="text-muted">Submit Your Hub </h1>
+        <p>Enter your information for distribution in our VR content syndication network.</p>
+        
+        
+         <form action="mail-handler.php" method="post">  
+    
+<div class="form-group" name="name">
+   Name: <input type="text" name="name"><br>
+</div> 
+    
 
-#echo $_POST["email"]; 
-echo $_POST["link"];
+<div class="form-group" name="hub_name">
+   Hub Name: <input type="text" name="hub_name"><br>
+</div>
 
-?>
+
+<div class="form-group" name="iframe">
+   Iframe: <input type="text" name="iframe"><br>
+</div>
+
+<div class="form-group">
+    Email: <input type="text" name="email"><br><br>
+</div>
+                               <button type="submit" class="btn btn-danger btn-lg" name="submit" value="Submit">Submit</button>
+</form>
+        
+        
+        <?php
+        echo $_POST["link"]
+        ?>
+     
+        
+    </div>
+    
+    </section>
+
+
 </div>
 </body>
 </html>
---
+++ mail-handler.php
@@ -0,0 +1,22 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "austinbarrington@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $hub_name = $_POST['hub_name'];
+    $iframe = $_POST['iframe'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = "Here is a copy of your message " . $name . "\n\n" . $_POST['iframe'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['iframe'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    header('Location: thank-you.html');
+exit();
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
